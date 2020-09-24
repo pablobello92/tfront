bbcf925ea562e5f0eeca8ce73588187777261afe
pablo-bello <77sof67@gmail.com>
2020-09-23 23:16:24 -0300

    UserTracks component modified

--- src/app/components/outlets/user/tracks/user-tracks.component.ts
+++ src/app/components/outlets/user/tracks/user-tracks.component.ts
@@ -1,6 +1,7 @@
 import {
     Component,
-    OnInit
+    OnInit,
+    ViewEncapsulation
 } from '@angular/core';
 
 import {
@@ -9,39 +10,41 @@ import {
 import {
     TracksService
 } from 'src/app/shared/services/tracksService';
+import {
+    ColorsService
+} from '../../../../shared/services/colorsService';
+
+import { MapOptions } from '../../../../shared/interfaces/MapOptions';
+import { NumericLimit, CombinedLimit } from '../../../../shared/interfaces/Limit';
+import { Range } from '../../../../shared/interfaces/Range';
+import { pipe } from 'rxjs';
+import { tap, map } from 'rxjs/operators';
+import { RangesAndLimits } from '../../../../shared/interfaces/RangesAndLimits';
+import { Line } from '../../../../shared/interfaces/Line';
+import { ColorInfoWidgetComponent } from './color-info-widget/color-info-widget.component';
 
 declare var google: any;
-
 @Component({
     selector: 'app-user-tracks',
     templateUrl: './user-tracks.component.html',
-    styleUrls: ['./user-tracks.component.scss']
+    styleUrls: ['./user-tracks.component.scss'],
+    encapsulation: ViewEncapsulation.None
 })
 export class UserTracksComponent implements OnInit {
 
-    options: any;
+    options: MapOptions;
     overlays = [];
-    userName;nickName: string;
+    userName: string;
+    nickName: string;
     deltas = 0;
     deltasBySegment = [];
-
     maxScore = 0;
 
-
-    private readonly COLOR_NOEVENT = '#00ffff'; // cyan
-    private readonly COLOR_VERYLOW = '#00ff00'; // green
-    private readonly COLOR_LOW = '#ffff00'; // yellow
-    private readonly COLOR_MEDIUM = '#ff8000'; // orange
-    private readonly COLOR_HIGH = '#ff0000'; // red
-    private readonly COLOR_VERYHIGH = '#8000ff'; // purple
-
-    private veryLowLimit: number;
-    private lowLimit: number;
-    private mediumLimit: number;
-    private highLimit: number;
+    public segmentLimits: CombinedLimit;
 
     constructor(
-        private _tracks: TracksService
+        private _tracks: TracksService,
+        private _colors: ColorsService
     ) {}
 
     ngOnInit() {
@@ -57,55 +60,43 @@ export class UserTracksComponent implements OnInit {
         };
     }
 
-    public fetchOne(): void {
-        this._tracks.getTracks(this.userName).subscribe(
-            res => {
-                console.clear();
-                console.log(res);
-                this.maxScore = this.getMaxValue(res.ranges);
-                this.drawLines(res.ranges);
-            }, err => {
-                console.error(err);
-            });
-    }
-
-    public fetchAll(): void {
-        this._tracks.getAllTracks(this.userName).subscribe(
-            ranges => {
-                console.clear();
-                this.maxScore = this.getMaxValue(ranges);
-                console.log(ranges);
-                this.drawLines(ranges);
-            }, err => {
-                console.error(err);
-            });
-    }
-
-    private drawLines(ranges: any[]): void { // TODO: ver si puedo descartar los ejes x,y de los datos de acelerometro
-        this.getEventScoreScale(ranges);
-        ranges.forEach(element => {
-            const line = new google.maps.Polyline({
-                path: [{
-                        lat: element.start.latitude,
-                        lng: element.start.longitude
-                    },
-                    {
-                        lat: element.end.latitude,
-                        lng: element.end.longitude
-                    }
-                ],
-                geodesic: true,
-                strokeColor: this.getColor(element.score),
-                strokeOpacity: 1,
-                strokeWeight: 4
-            });
-            this.overlays.push(line);
+    public fetchTracks(): void {
+        this._tracks.getTracks(this.userName)
+        .subscribe((ranges: Range[]) => {
+            this.maxScore = this.getMaxValue(ranges);
+            this.segmentLimits = this.setRelativeScoreScale(ranges);
+            this.overlays = ranges.map((range: Range) =>  this.mapRangeToPolyLine(range));
+        }, err => {
+            console.error(err);
         });
-        return;
+    }
+
+    /**
+     * MAYBE THIS FUNCTIONALITY SHOULD GO IN A SEPARATE FUNCTION FILE
+    */
+    /*
+    * TODO: ver si puedo descartar los ejes x,y de los datos de acelerometro
+    */
+    private mapRangeToPolyLine(range: Range): any {
+        return new google.maps.Polyline({
+            path: [{
+                lat: range.start.lat,
+                lng: range.start.lng
+            },
+            {
+                lat: range.end.lat,
+                lng: range.end.lng
+            }
+            ],
+            geodesic: true,
+            strokeColor: this._colors.getColor(range.score, this.segmentLimits),
+            strokeOpacity: 1,
+            strokeWeight: 4
+        });
     }
 
     // Obtengo el score maximo para asi armar los rangos de colores (AL FINAL NO LO USÉ)
-    private getMaxValue(ranges: any[] ): number {
+    private getMaxValue(ranges: Range[]): number {
         let max = 0;
         ranges.forEach(element => {
             if (element.score > max) {
@@ -115,8 +106,7 @@ export class UserTracksComponent implements OnInit {
         return max;
     }
 
-    private getEventScoreScale(ranges: any[]): void {
-
+    private setRelativeScoreScale(ranges: Range[]): CombinedLimit {
         let min = 100000;
         let max = 0;
         let avg = 0;
@@ -132,34 +122,14 @@ export class UserTracksComponent implements OnInit {
             }
         });
         avg = avg / counter;
-
         const lowerStep = (avg - min) / 4;
         const upperStep = (max - avg) / 4;
-
-        this.veryLowLimit = min + lowerStep;
-        this.lowLimit = avg - lowerStep;
-        this.mediumLimit = avg + upperStep;
-        this.highLimit = max - upperStep;
-
-        return;
-    }
-
-    private getColor(score: number): string {
-        if (score === 0) {
-            return this.COLOR_NOEVENT;
-        }
-        if (score < this.veryLowLimit) {
-            return this.COLOR_VERYLOW;
-        }
-        if (score < this.lowLimit) {
-            return this.COLOR_LOW;
-        }
-        if (score < this.mediumLimit) {
-            return this.COLOR_MEDIUM;
-        }
-        if (score < this.highLimit) {
-            return this.COLOR_HIGH;
-        }
-        return this.COLOR_VERYHIGH;
+        const numericLimit = {
+            veryLow: min + lowerStep,
+            low: avg - lowerStep,
+            medium: avg + upperStep,
+            high: max - upperStep
+        };
+        return this._colors.getCombinedLimits(numericLimit);
     }
 }
