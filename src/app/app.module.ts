6081eee85a7b59a51bf9c27d682359b30b0c951b
pablobello92 <77soft67@gmail.com>
2021-08-26 16:07:20 -0300

    Just reordered the imports, added the theme to styles.scss, removed the useless library quill.js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,62 @@ import {
     HttpClient
 } from '@angular/common/http';
 import {
+    CookieService
+} from 'ngx-cookie-service';
+
+import {
+    AgmCoreModule
+} from '@agm/core';
+import {
+    MatButtonModule
+} from '@angular/material/button';
+import {
+    MatButtonToggleModule
+} from '@angular/material/button-toggle';
+import {
+    MatChipsModule
+} from '@angular/material/chips';
+import {
+    MatDatepickerModule
+} from '@angular/material/datepicker';
+import {
+    MatFormFieldModule
+} from '@angular/material/form-field';
+import {
+    MatIconModule
+} from '@angular/material/icon';
+import {
+    MatInputModule
+} from '@angular/material/input';
+import {
+    MatNativeDateModule
+} from '@angular/material/core';
+import {
+    MatListModule
+} from '@angular/material/list';
+import {
+    MatSelectModule
+} from '@angular/material/select';
+import {
+    MatSnackBarModule
+} from '@angular/material/snack-bar';
+import {
+    MatTooltipModule
+} from '@angular/material/tooltip'
+
+import {
+    NavbarComponent
+} from './components/layout/navbar/navbar.component';
+import {
+    TranslateModule,
+    TranslateLoader,
+    TranslateService
+} from '@ngx-translate/core';
+import {
+    TranslateHttpLoader
+} from '@ngx-translate/http-loader';
+
+import {
     AppRoutingModule
 } from './app-routing.module';
 import {
@@ -48,54 +104,6 @@ import {
 import {
     SideBarComponent
 } from './components/layout/sidebar/sidebar.component';
-
-import {
-    AgmCoreModule
-} from '@agm/core';
-import {
-    MatFormFieldModule
-} from '@angular/material/form-field';
-import {
-    MatSelectModule
-} from '@angular/material/select';
-import {
-    MatListModule
-} from '@angular/material/list';
-import {
-    MatDatepickerModule
-} from '@angular/material/datepicker';
-import {
-    MatNativeDateModule
-} from '@angular/material/core';
-import {
-    MatIconModule
-} from '@angular/material/icon';
-import {
-    MatTooltipModule
-} from '@angular/material/tooltip'
-import {
-    MatInputModule
-} from '@angular/material/input';
-import {
-    MatSnackBarModule
-} from '@angular/material/snack-bar';
-import {
-    MatButtonToggleModule
-} from '@angular/material/button-toggle';
-import {
-    NavbarComponent
-} from './components/layout/navbar/navbar.component';
-import {
-    TranslateModule,
-    TranslateLoader,
-    TranslateService
-} from '@ngx-translate/core';
-import {
-    TranslateHttpLoader
-} from '@ngx-translate/http-loader';
-import {
-    CookieService
-} from 'ngx-cookie-service';
 import {
     LoginComponent
 } from './components/pages/login/login.component';
@@ -170,27 +178,32 @@ export function createTranslateLoader(http: HttpClient) {
                 deps: [HttpClient]
             }
         }),
-        MatFormFieldModule,
-        MatInputModule,
-        MatSelectModule,
+        FormsModule,
+        MatButtonModule,
         MatButtonToggleModule,
-        MatListModule,
-        MatTooltipModule,
-        MatIconModule,
-        MatSnackBarModule,
+        MatChipsModule,
         MatDatepickerModule,
+        MatFormFieldModule,
+        MatIconModule,
+        MatInputModule,
+        MatListModule,
         MatNativeDateModule,
-        FormsModule
+        MatSelectModule,
+        MatSnackBarModule,
+        MatTooltipModule
     ],
     exports: [
+        MatButtonModule,
+        MatButtonToggleModule,
+        MatChipsModule,
+        MatDatepickerModule,
         MatFormFieldModule,
-        MatInputModule,
-        MatSelectModule,
-        MatListModule,
         MatIconModule,
+        MatInputModule,
+        MatListModule,
+        MatSelectModule,
         MatSnackBarModule,
-        MatTooltipModule,
-        MatDatepickerModule
+        MatTooltipModule
     ],
     providers: [
         CommonService,
