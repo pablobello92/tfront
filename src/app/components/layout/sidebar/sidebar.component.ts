64bac5b81b817d12d564c3f66d34ca3cd5332703
pablobello92 <77soft67@gmail.com>
2021-08-30 18:28:43 -0300

    Fixed new sumarized-data view route

--- src/app/components/layout/sidebar/sidebar.component.ts
+++ src/app/components/layout/sidebar/sidebar.component.ts
@@ -64,7 +64,7 @@ export class SideBarComponent implements OnInit {
             {
                 label: translations.titles.sumarized_data,
                 icon: 'analytics',
-                routerLink: ['/sumarized-tracks'],
+                routerLink: ['/sumarized-data'],
                 enabled: true
             },
             {
